f0a1016f49a9912e0b0f8d3239f9f574e38f0f84
Ale <alejandrorobledo.00@gmail.com>
2025-12-12 18:54:27 -0300

    network-stage fix

--- lib/network/network-stage.ts
+++ lib/network/network-stage.ts
@@ -1,15 +1,16 @@
-import { Stage, StageProps } from "aws-cdk-lib";
-import { NetworkStack } from "./network-stack";
+import * as cdk from 'aws-cdk-lib';
+import { NetworkStack } from './network-stack';
 
-export class NetworkStage extends Stage {
+export class NetworkStage extends cdk.Stage {
   public readonly vpcOutput;
   public readonly albOutput;
   public readonly listenerOutput;
 
-  constructor(scope: any, id: string, props?: StageProps) {
+  constructor(scope: cdk.App, id: string, props?: cdk.StageProps) {
     super(scope, id, props);
 
-    const net = new NetworkStack(this, "NetworkStack", props);
+    // Pass 'scope' as the first argument, not 'this'
+    const net = new NetworkStack(scope, "NetworkStack", props);
 
     this.vpcOutput = net.vpcOutput;
     this.albOutput = net.albOutput;
