d6e7c82960adc0f4df36069223c60e58e134318e
Ale <alejandrorobledo.00@gmail.com>
2025-12-14 19:18:49 -0300

    fix pipeline-stack

--- lib/pipeline-stack (copy).ts
+++ lib/pipeline-stack (copy).ts
@@ -6,46 +6,57 @@ import {
 } from "aws-cdk-lib/pipelines";
 
 import { DockerEcrStage } from "./docker-ecr/docker-ecr-stage";
+
 import { AppDeployStage } from "./app-deploy/app-deploy-stage";
+
+import { SecretValue } from "aws-cdk-lib";
+
+
 import { Construct } from "constructs";
 
 export class PipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const env = {
-      account: this.account,
-      region: this.region,
-    };
-
     const pipeline = new CodePipeline(this, "Pipeline", {
       pipelineName: "FullInfraPipeline",
       synth: new ShellStep("Synth", {
-        input: CodePipelineSource.connection(
-          "alexriley7/codepipeline_a",
-          "main",
+
+        input: CodePipelineSource.connection("alexriley7/codepipeline_a", "main",
+          
           {
-            connectionArn:
+
+          //authentication: SecretValue.secretsManager("github-token3"),
+          connectionArn:
               "arn:aws:codeconnections:us-east-1:456582263462:connection/f057400e-c5b1-4801-ac26-ddcfdd5f4a30",
-          }
-        ),
+
+        }),
+
         commands: ["npm ci", "npm run build", "npx cdk synth"],
       }),
     });
 
-    // ✅ Docker image build stage (MUST have env)
-    const dockerEcrStage = new DockerEcrStage(this, "DockerEcrStage", {
-      env,
-    });
+
+    // working code didnt have dockerEcrStage as a variable here
+
+    const dockerEcrStage = new DockerEcrStage(this, "DockerEcrStage");
 
     pipeline.addStage(dockerEcrStage);
-
-    // ✅ ECS deploy stage
+    
     pipeline.addStage(
       new AppDeployStage(this, "AppDeployStage", {
-        env,
         imageUri: dockerEcrStage.imageUri,
+        env: {
+          account: this.account,
+          region: this.region,
+        },
       })
     );
+
+    console.log("Account:", this.account);
+    console.log("Region:", this.region);
   }
 }
+
+
+
